f4674da9a700b24a8be09bb610c7e78143ffccdf
zakaria <elattarz@yahoo.com>
2022-11-04 15:54:17 +0100

    video support

--- src/Conference.js
+++ src/Conference.js
@@ -511,16 +511,16 @@ export default function Conference(props) {
     const getLocalStream = () => {
       navigator.mediaDevices.getUserMedia({
         audio: true,
-        // video: {
-        //   width: {
-        //     min: 640,
-        //     max: 1920,
-        //   },
-        //   height: {
-        //     min: 400,
-        //     max: 1080,
-        //   }
-        // }
+        video: {
+          width: {
+            min: 640,
+            max: 1920,
+          },
+          height: {
+            min: 400,
+            max: 1080,
+          }
+        }
       })
         .then(streamSuccess)
         .catch(error => {
